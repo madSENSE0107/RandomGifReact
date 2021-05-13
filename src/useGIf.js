3a224f16bbe8d619b4531cb6ac36e98a3402a8b3
madSENSE0107 <saptarsi2000@gmail.com>
2021-05-13 08:39:11 +0530

    2nd commit to fix missing dependency error

--- src/useGIf.js
+++ src/useGIf.js
@@ -1,19 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 const API_KEYY=process.env.REACT_APP_GIPHY_KEY;
-const url= `https://api.giphy.com/v1/gifs/random?api_key=${API_KEYY}`;
+// const url= `https://api.giphy.com/v1/gifs/random?api_key=${API_KEYY}`;
 
-const useGif = (khoj) => {
+const useGif = (tag) => {
     const [gif,setGif]=useState("")
-    const fetchGif=async()=>{
-        const info=await axios.get(khoj?`${url}&tag=${khoj}` : url);
-        const imageSrc=info.data.data.images.downsized_large.url;
-        setGif(imageSrc)
-        console.log(imageSrc);
+    
+ 
+    const fetchGif = async (tag) => {
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEYY}&tag=${tag}`;
+
+        const { data } = await axios.get(url);
+
+        const imageSrc = data.data.images.downsized_large.url;
+
+        setGif(imageSrc);
     }
-    useEffect(()=>{     
-        fetchGif(khoj);
-    },[khoj])
+
+    useEffect(() => {
+        fetchGif(tag);
+    }, [tag]);
+
 
     return {gif,fetchGif};
 }
